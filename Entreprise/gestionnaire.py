1bb2d88363f58809bce6628a4d9752c230d493a2
Aymen El yaakoubi <aymaneyaakoubi528@gmail.com>
2026-04-09 00:21:45 +0100

    Add files via upload

---
+++ Entreprise/gestionnaire.py
@@ -0,0 +1,10 @@
+from employe import Employe
+
+class Gestionnaire(Employe):
+    def __init__(self, nom, prenom, salaire, departement):
+        super().__init__(nom, prenom, salaire)
+        self.departement = departement
+
+    def afficher_infos(self):
+        super().afficher_infos()
+        print(f"Département: {self.departement}")
