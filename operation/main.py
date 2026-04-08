774df0748413a2361637d1cc69aa8d7e1501376e
Aymen El yaakoubi <aymaneyaakoubi528@gmail.com>
2026-04-09 00:21:17 +0100

    Add files via upload

---
+++ operation/main.py
@@ -0,0 +1,6 @@
+from math_operations import operations
+
+print("Addition:", operations.addition(10, 5))
+print("Soustraction:", operations.soustraction(10, 5))
+print("Multiplication:", operations.multiplication(10, 5))
+print("Division:", operations.division(10, 5))
