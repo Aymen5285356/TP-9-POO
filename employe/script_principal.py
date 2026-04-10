1c0a7c0e31e1f0c8ed5db6394b208ee91c16a7b1
Aymen El yaakoubi <aymaneyaakoubi528@gmail.com>
2026-04-10 11:56:23 +0100

    Update script_principal.py

--- employe/script_principal.py
+++ employe/script_principal.py
@@ -1,15 +1,12 @@
 from employe import Employe
 from entreprise.gestionnaire import Gestionnaire
 
-# Création d'employés
 emp1 = Employe("Dupont", "Jean", 3000)
 emp2 = Employe("Martin", "Sophie", 3500)
 
-# Création de gestionnaires
 gest1 = Gestionnaire("Durand", "Paul", 5000, "Informatique")
 gest2 = Gestionnaire("Lemoine", "Claire", 5500, "RH")
 
-# Affichage
 print("=== Employés ===")
 emp1.afficher_infos()
 print()
